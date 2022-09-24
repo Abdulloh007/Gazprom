c71e0d342adfc197ea4fd5ae596540e36eadfb40
Abdulloh007 <sabirovabdulloh2004@gmail.com>
2022-09-24 16:11:03 +0300

    Done

--- src/js/useful__gazprom-privilege.js
+++ src/js/useful__gazprom-privilege.js
@@ -26,3 +26,23 @@ function activeTabination(elem) {
 if (tabination !== undefined && tabination !== null) {
     tabination.forEach(item => activeTabination(item));
 }
+//FAQ
+const faqs = document.querySelectorAll('.g-faq');
+console.log(faqs);
+
+faqs.forEach(item => {
+    let currentHeight = item.clientHeight;
+    let collapseHeight = item.querySelector('.g-faq__header').clientHeight + 30;
+    const btn = item.querySelector('.g-btn');
+    item.style.height = collapseHeight + 'px';
+    
+    btn.addEventListener('click', (e) => {
+        if (item.classList.contains('active')) {
+            item.classList.remove('active');
+            item.style.height = collapseHeight + 'px';
+        }else {
+            item.classList.add('active');
+            item.style.height = currentHeight + 'px';
+        }
+    })
+})
